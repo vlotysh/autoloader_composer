9f76352be82cda05fa8d7b47b30c5b51f2443a84
vlotysh <lotysh.vm@gmail.com>
2015-09-02 02:01:30 +0300

    fix all

--- core/Core.php
+++ core/Core.php
@@ -8,16 +8,60 @@
 
 namespace vlad;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
 /**
  * Description of Core
  *
  * @author vlad
  */
-class Core {
+class Core implements HttpKernelInterface {
+
+    protected $routes = array();
     
-    public function __construct($config = null) {
-        echo 'CORE';
-        
+    public function __construct()
+    {
+        $this->routes = new RouteCollection();
     }
     
+    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)     {
+        // создаем контекст, используя данные запроса
+        $context = new RequestContext();
+        $context->fromRequest($request);
+
+        $matcher = new UrlMatcher($this->routes, $context);
+
+        try {
+            $attributes = $matcher->match($request->getPathInfo());
+            $arguments = $attributes;
+            unset($arguments['_route']);
+            unset($arguments['controller']);
+            
+            $controller = $attributes['controller'];
+            $function = new \ReflectionFunction($controller);
+
+            $response = $function->invokeArgs($arguments);
+        } catch (ResourceNotFoundException $e) {
+            $response = new Response('Не найден!', Response::HTTP_NOT_FOUND);
+        }
+
+        return $response;
+    }
+
+    public function map($path, $controller) {
+
+        $this->routes->add($path, new Route(
+            $path,
+            array('controller' => $controller)
+        ));
+    }
+
 }
--- index.php
+++ index.php
@@ -1,10 +1,54 @@
-<?php 
+<?php
 
-require_once 'vendor/autoload.php';
+$loader = require 'vendor/autoload.php';
+$loader->register();
 
-use \application\models\User;
 
 $config = array();
 $core = new \vlad\Core($config);
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$response = new Response();
+
+
+
+$core->map('/', function () {
+    return new Response('home');
+});
+
+$core->map('/hello/{name}/{age}', function ($name) {
+
+    return new Response('Hello '.$name);
+});
+
+
+$response = $core->handle($request);
+$response->send();
+
+/*
+$locator = new FileLocator(array(__DIR__));
+$loader = new YamlFileLoader($locator);
+$collection = $loader->load('src'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'routes.yml');
+
+
+$route = new Route('/foo', array('controller' => 'MyController'));
+$routes = new RouteCollection();
+$routes->add('route_name', $route);
+
+$context = new RequestContext($_SERVER['REQUEST_URI']);
+
+$matcher = new UrlMatcher($routes, $context);
+
+$parameters = $matcher->match('/foo');
+var_dump($matcher);
+
+$config = array();
+
+
 $core = new User();
+ * 
+ * /
+ */
